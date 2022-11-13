0a6e20083e362fa204c5806abc90669301354be1
Andrey Drozdov <eniac01@yandex.ru>
2022-11-13 22:34:44 +0300

    primary commit task2

---
+++ task4/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 
+*/
+
+Console.Write("enter number 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("enter number 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("enter number 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+    if(a<b) max = b;
+    if(b<c) max = c; 
+    
+Console.WriteLine("result: " + max);
