82a15780f54a90bc95374f8b8e7da9c658d523a3
Andrey Drozdov <eniac01@yandex.ru>
2022-11-13 22:42:45 +0300

    primary commit task6

---
+++ task6/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+
+*/
+
+Console.Write("enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b = a % 2;
+ 
+if (b==1)
+{
+    Console.WriteLine("no");
+}
+else 
+{
+    Console.WriteLine("yes");
+}
+
+
+
+
