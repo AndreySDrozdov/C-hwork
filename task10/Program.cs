76c0284992990b7a865d100c75f4f911c58de324
Andrey Drozdov <eniac01@yandex.ru>
2022-11-17 21:44:47 +0300

    first commit task10

---
+++ task10/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int randomNumber = new Random().Next(100,1000);
+int lastDigit = randomNumber % 10;
+int firstDigit = randomNumber / 10;
+int thirdDigit = firstDigit % 10; 
+
+Console.WriteLine(randomNumber);
+
+Console.WriteLine(thirdDigit);
