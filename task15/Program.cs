051247c0b48e9daac37ca7c6bc1afe591ee1c220
Andrey Drozdov <eniac01@yandex.ru>
2022-11-17 21:49:14 +0300

    first commit task15

---
+++ task15/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет 
+*/
+
+int dayNumber = ReadInt("введи номер дня недели: ");
+
+if (dayNumber == 1 || dayNumber == 2 || dayNumber == 3 || dayNumber == 4 || dayNumber ==5)
+ {
+     Console.WriteLine("не выходной");
+ }
+else if ( dayNumber == 6 || dayNumber == 7)
+ {
+    Console.WriteLine("ура, выходной");
+ }
+else
+    {
+       Console.WriteLine("в неделе всего 7 дней!"); 
+    }
+        
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
